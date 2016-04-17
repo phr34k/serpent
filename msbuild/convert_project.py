c2003f40b0ac8deeef8490b971f5070ce41d85d3
Lawrence Kok <phr34k@users.noreply.github.com>
2016-04-17 17:02:12 +0200

    Converts a vcxproj to serpent project definition.

---
+++ msbuild/convert_project.py
@@ -0,0 +1,52 @@
+import clr, sys
+clr.AddReference("Microsoft.Build")
+clr.AddReference("Microsoft.Build.Framework")
+import Microsoft.Build.Evaluation
+import System
+
+def main(project):
+    collection = Microsoft.Build.Evaluation.ProjectCollection();
+    collection.DefaultToolsVersion = "4.0"
+    project = collection.LoadProject(project);
+
+    includes = ''
+    libs = ''
+    defines = ''
+    outdir = project.GetPropertyValue('OutDir')
+    intdir = project.GetPropertyValue('IntDir')
+    for y in project.ItemDefinitions:
+        if y.ItemType == "ClCompile":
+            includes = y.GetMetadataValue("AdditionalIncludeDirectories")            
+            defines = y.GetMetadataValue("PreprocessorDefinitions")
+        if y.ItemType == "Link":
+            libs = y.GetMetadataValue("AdditionalDependencies")        
+
+    print "msbuild_ext.visual_studio("
+    print "\tproject = \"\","
+    print "\tfiles = ["            
+    for x in project.GetItems("ClCompile"):
+        print "\t\t'" + x.EvaluatedInclude + "',"
+    print "\t],"
+
+    print "\tincludes = ["            
+    for y in includes.split(';'):
+        print "\t\t'" + y + "',"
+    print "\t],"
+
+    print "\tlibs = ["            
+    for y in libs.split(';'):
+        print "\t\t'" + y + "',"
+    print "\t],"    
+
+    print "\tname = 'new',"
+    print "\tdefines = ["            
+    for y in defines.split(';'):
+        print "\t\t'" + y + "',"
+    print "\t],"  
+    print "\toutdir = r'%s'," % outdir
+    print "\tintdir = r'%s'" % intdir
+    print ")"
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
