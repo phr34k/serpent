17f9908db91a655ce379a100d3427eacff5c777b
Lawrence Kok <phr34k@users.noreply.github.com>
2016-04-24 20:20:54 +0200

    Update serpent.py

--- serpent/serpent.py
+++ serpent/serpent.py
@@ -1,3 +1,7 @@
+import os
+path=os.path
+del os
+
 _prebuild = []
 _postbuild = []
 _targets = targets;
@@ -22,4 +26,25 @@ def postbuild(func):
 def package(name, packages = []):   
    if name in targets:
       _targets = list(set(_targets + packages))
-   target( name = name )   
+   target( name = name )
+
+def download(url, hash = None):
+   import urllib2
+   url = url
+   file_name = url.split('/')[-1]
+   u = urllib2.urlopen(url)
+   f = open(file_name, 'wb')
+   meta = u.info()
+   file_size = int(meta.getheaders("Content-Length")[0])
+   file_size_dl = 0
+   block_sz = 8192
+   while True:
+       buffer = u.read(block_sz)
+       if not buffer:
+           break
+       file_size_dl += len(buffer)
+       f.write(buffer)
+       status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+       status = status + chr(8)*(len(status)+1)
+       print status,
+   f.close()   
