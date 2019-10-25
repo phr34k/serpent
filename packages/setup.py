9614a687fd960ce909dfa947c13c2bd4300f5288
Lawrence Kok <lawrence.kok@basemark.com>
2019-10-25 14:19:49 +0700

    Refactored into python packages.

---
+++ packages/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+     name='serpent',  
+     version='0.2',
+     description="A msbuild conversion utility to the serpent specification",
+     long_description="A msbuild conversion utility to the serpent specification",
+     url="https://github.com/phr34k/serpent",
+     packages=['serpent-msbuild'],
+     include_package_data=True,     
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: Windows",
+     ],
+      entry_points={
+          # 'console_scripts': [
+          #     'srp-convert=serpent-msbuild.convert_project.main',
+          # ]
+      },     
+ )
