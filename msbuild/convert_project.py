39f600d44f5692dd19a92a2e9743e0d4750a61bf
Lawrence Kok <phpfreaked9@gmail.com>
2021-12-03 19:38:21 +0100

    Update convert_project.py
    changed backslashes to forward slashes.

--- msbuild/convert_project.py
+++ msbuild/convert_project.py
@@ -25,17 +25,17 @@ def main(project):
     print "\tproject = \"\","
     print "\tfiles = ["            
     for x in project.GetItems("ClCompile"):
-        print "\t\t'" + x.EvaluatedInclude + "',"
+        print "\t\t'" + x.EvaluatedInclude.replace("\\","/") + "',"
     print "\t],"
 
     print "\tincludes = ["            
     for y in includes.split(';'):
-        print "\t\t'" + y + "',"
+        print "\t\t'" + y.replace("\\","/") + "',"
     print "\t],"
 
     print "\tlibs = ["            
     for y in libs.split(';'):
-        print "\t\t'" + y + "',"
+        print "\t\t'" + y.replace("\\","/") + "',"
     print "\t],"    
 
     print "\tname = 'new',"
@@ -43,8 +43,8 @@ def main(project):
     for y in defines.split(';'):
         print "\t\t'" + y + "',"
     print "\t],"  
-    print "\toutdir = r'%s'," % outdir
-    print "\tintdir = r'%s'" % intdir
+    print "\toutdir = r'%s'," % outdir.replace("\\","/")
+    print "\tintdir = r'%s'" % intdir.replace("\\","/")
     print ")"
 
 if __name__ == '__main__':
