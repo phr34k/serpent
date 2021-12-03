aef72db8d48cf7aa65863f58c386aff38d49570c
Lawrence Kok <phpfreaked9@gmail.com>
2021-12-03 19:36:35 +0100

    Update compact.py

--- msbuild/compact.py
+++ msbuild/compact.py
@@ -1,20 +1,35 @@
-import os, sys, re
+import os, sys, re, shutil
 
 def verify(path):
-	os.remove(path)	
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+    else:
+        os.remove(path)
 
 def scan(path):
-	for subdir, dirs, files in os.walk(path):
-	    for file in files:
-	        filepath = subdir + os.sep + file        
-	        if filepath.endswith(".ncb"):
-	            verify(filepath)
-	        if filepath.endswith(".sdf"):
-	        	verify(filepath)
-	        if filepath.endswith(".opensdf"):
-	        	verify(filepath)
+    for subdir, dirs, files in os.walk(path):
+        for dir in dirs:
+            filepath = subdir + os.sep + dir
+            if filepath.endswith("_UpgradeReport_Files"):
+                verify(filepath)
+            if filepath.endswith("Backup"):
+                verify(filepath)
+        for file in files:
+            filepath = subdir + os.sep + file
+            if filepath.endswith(".ncb"):
+                verify(filepath)
+            if filepath.endswith(".sdf"):
+                verify(filepath)
+            if filepath.endswith(".opensdf"):
+                verify(filepath)
+            if filepath.endswith("UpgradeLog.XML"):
+                verify(filepath)
+            if filepath.endswith("UpgradeLog.htm"):
+                verify(filepath)
 
 
 if __name__ == "__main__":
-	if len(sys.argv) >= 1:
-		scan(sys.argv[1])
+    if len(sys.argv) > 1:
+        scan(sys.argv[1])
+    else:
+        scan(".")
